02de16cd36a4bba449941de36a70f84fe3bb7ff1
igcarvalhaes <igcarvalhaes@hotmail.com>
2024-07-09 12:52:59 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const atividade = {
+  nome: "Almoço",
+  data: new Date("2024-07-08 10:00"),
+  finalizada: true,
+};
+
+const atividades = [
+  atividade,
+  {
+    nome: "Academia em grupo",
+    data: new Date("2024-07-08 12:00"),
+    finalizada: false,
+  },
+  {
+    nome: "Gaming session",
+    data: new Date("2024-07-08 12:00"),
+    finalizada: true,
+  },
+];
+
+const criarItemDeAtividade = (atividade) => {
+  let input = '<input type="checkbox" ';
+
+  if (atividade.finalizada) {
+    input += "checked";
+  }
+
+  input += ">";
+
+  return `
+     <div>
+        ${input}
+        <span>${atividade.nome}</span>
+        <time>${atividade.data}</time>
+      </div>
+    `;
+};
+
+const section = document.querySelector("section");
+
+for (let atividade of atividades) {
+  section.innerHTML += criarItemDeAtividade(atividade);
+}
